02aa1164e171380cda30610084eb09ee1785c3e7
Jon Palacios <jonatangpalacios@gmail.com>
2022-07-06 12:28:42 +0200

    face_control v1

---
+++ face_control.py
@@ -0,0 +1,135 @@
+import mediapipe as mp
+import pandas as pd
+import pickle
+import cv2
+import numpy as np
+import imutils
+from djitellopy import tello
+import time
+import requests
+
+# Connect to drone via AP (Specifiy your drone IP)
+tello_ip = "192.168.0.30"  # Tello IP address
+
+drone = tello.Tello(host=tello_ip)
+drone.connect()
+print(drone.get_battery())
+time.sleep(2)
+
+# Replace the below URL with your own. Make sure to add "/shot.jpg" at last.
+url = "http://192.168.0.22:8080/shot.jpg"
+
+mp_drawing = mp.solutions.drawing_utils  # Drawing helpers
+mp_holistic = mp.solutions.holistic  # Mediapipe Solutions
+
+with open('modules/body_language.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+# Initiate holistic model
+with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+    count = 0
+    while True:
+        # Get image from phone
+        img_resp = requests.get(url)
+        img_arr = np.array(bytearray(img_resp.content), dtype=np.uint8)
+        image = cv2.imdecode(img_arr, -1)
+        image = imutils.resize(image, width=640, height=480)
+
+        # Make Detections
+        results = holistic.process(image)
+
+        # Recolor image back to BGR for rendering
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        # 1. Draw face landmarks
+        mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION,
+                                  mp_drawing.DrawingSpec(color=(80, 110, 10), thickness=1, circle_radius=1),
+                                  mp_drawing.DrawingSpec(color=(80, 256, 121), thickness=1, circle_radius=1)
+                                  )
+
+        # 2. Right hand
+        mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                                  mp_drawing.DrawingSpec(color=(80, 22, 10), thickness=2, circle_radius=4),
+                                  mp_drawing.DrawingSpec(color=(80, 44, 121), thickness=2, circle_radius=2)
+                                  )
+
+        # 3. Left Hand
+        mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                                  mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
+                                  mp_drawing.DrawingSpec(color=(121, 44, 250), thickness=2, circle_radius=2)
+                                  )
+
+        # 4. Pose Detections
+        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
+                                  mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=4),
+                                  mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
+                                  )
+        # Export coordinates
+        try:
+            # Extract Pose landmarks
+            pose = results.pose_landmarks.landmark
+            pose_row = list(
+                np.array([[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in pose]).flatten())
+
+            # Extract Face landmarks
+            face = results.face_landmarks.landmark
+            face_row = list(
+                np.array([[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in face]).flatten())
+
+            # Concatenate rows
+            row = pose_row + face_row
+
+            # Make Detections
+            X = pd.DataFrame([row])
+            body_language_class = model.predict(X)[0]
+            body_language_prob = model.predict_proba(X)[0]
+            # print(body_language_class, body_language_prob)
+            if body_language_class == "BigSmile":
+                count = count + 1
+                if count == 20:
+                    # Take off drone
+                    drone.takeoff()
+
+
+            # Grab ear coords
+            coords = tuple(np.multiply(
+                np.array(
+                    (results.pose_landmarks.landmark[mp_holistic.PoseLandmark.LEFT_EAR].x,
+                     results.pose_landmarks.landmark[mp_holistic.PoseLandmark.LEFT_EAR].y))
+                , [640, 480]).astype(int))
+
+            cv2.rectangle(image,
+                          (coords[0], coords[1] + 5),
+                          (coords[0] + len(body_language_class) * 20, coords[1] - 30),
+                          (245, 117, 16), -1)
+            cv2.putText(image, body_language_class, coords,
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
+            # Get status box
+            cv2.rectangle(image, (0, 0), (250, 60), (245, 117, 16), -1)
+
+            # Display Class
+            cv2.putText(image, 'CLASS'
+                        , (95, 12), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+            cv2.putText(image, body_language_class.split(' ')[0]
+                        , (90, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
+            # Display Probability
+            cv2.putText(image, 'PROB'
+                        , (15, 12), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+            cv2.putText(image, str(round(body_language_prob[np.argmax(body_language_prob)], 2))
+                        , (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
+        except:
+            pass
+
+        cv2.imshow('Phone Feed', image)
+
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+
+drone.land()
+cv2.destroyAllWindows()
+
+
