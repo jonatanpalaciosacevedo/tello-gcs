0baa937cec326501c922d353cbff5806dd720f59
Jon Palacios <jonatangpalacios@gmail.com>
2022-07-12 12:49:04 +0200

    Update ocr_drone.py v2

--- ocr_control/ocr_drone.py
+++ ocr_control/ocr_drone.py
@@ -12,12 +12,13 @@ drone = tello.Tello()
 drone.connect()
 print(f"Battery: {drone.get_battery()}%")
 drone.streamon()  # turn on camera
+frame_read = drone.get_frame_read()
 
 # Best resolution
-# drone.set_video_resolution(drone.RESOLUTION_720P)
+drone.set_video_resolution(drone.RESOLUTION_720P)
 
 # Most FPS
-# drone.set_video_fps(drone.FPS_30)
+drone.set_video_fps(drone.FPS_30)
 
 # path of pytesseract execution folder (Change as needed)
 # For windows, the path is usually in either of these two:
@@ -37,10 +38,12 @@ time.sleep(2.0)
 cntr = 0
 detecting = False
 
+drone.takeoff()
+
 # loop over frames from the video stream
 while True:
     # Drone camera
-    orig = drone.get_frame_read().frame
+    orig = frame_read.frame
 
     # grab the next frame and handle if we are reading from either
     # a webcam or a video file
@@ -64,40 +67,54 @@ while True:
     temp = f"Temperature: {drone.get_temperature()}C"
     bat = f"Battery: {drone.get_battery()}%"
 
-    cv2.putText(frame, thr, (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.7, color, 2)
+    """cv2.putText(frame, thr, (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.7, color, 2)
     cv2.putText(frame, temp, (10, 25 + 40), cv2.FONT_HERSHEY_SIMPLEX, 0.7, color, 2)
-    cv2.putText(frame, bat, (10, 25 + 40 + 40), cv2.FONT_HERSHEY_SIMPLEX, 0.7, color, 2)
+    cv2.putText(frame, bat, (10, 25 + 40 + 40), cv2.FONT_HERSHEY_SIMPLEX, 0.7, color, 2)"""
 
     # if "c" is press, start detecting
     if key == ord("c"):
         detecting = not detecting
+
+    if key == ord("w"):
+        drone.move_up(20)
+
+    if key == ord("s"):
+        drone.move_down(20)
+
+    if key == ord("d"):
+        drone.move_right(20)
+
+    if key == ord("a"):
+        drone.move_left(20)
 
     # process data if not blurry
     if not blurry and detecting:
         # Test detection threshold
         text = "Detecting..."
 
-        cv2.putText(frame, text, (10, 25 + 40 + 40 + 40), cv2.FONT_HERSHEY_SIMPLEX, 0.7, color, 2)
+        """cv2.putText(frame, text, (10, 25 + 40 + 40 + 40), cv2.FONT_HERSHEY_SIMPLEX, 0.7, color, 2)"""
 
         # Process letters here
         if (cntr % 20) == 0:
             img_h, img_w, _ = frame.shape
             x1, y1, w1, h1 = 0, 0, img_h, img_w
-            imgchar = pytesseract.image_to_string(frame)
+            imgchar = pytesseract.image_to_string(frame, config="--psm 10")
             imgboxes = pytesseract.image_to_boxes(frame)
-
-            print(imgchar)
 
             for boxes in imgboxes.splitlines():
                 boxes = boxes.split(" ")
                 x, y, w, h = int(boxes[1]), int(boxes[2]), int(boxes[3]), int(boxes[4])
                 cv2.rectangle(frame, (x, img_h - y), (w, img_h - h), (0, 0, 255), 1)
 
+            print(imgchar)
+
     # show the output video OCR visualization
     cv2.imshow("Output", frame)
 
     # if the 'q' key was pressed, break from the loop
     if key == ord("q"):
+        drone.land()
+        drone.streamoff()
         break
 
 
