589facfd25cd9b2ea8d19a110572ed918e13704d
Jon Palacios <jonatangpalacios@gmail.com>
2022-07-13 21:06:13 +0200

    Update face detection ML model

--- face_control/face_detect.py
+++ face_control/face_detect.py
@@ -3,6 +3,7 @@ import cv2
 import csv
 import numpy as np
 import pandas as pd
+import requests
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
@@ -33,10 +34,11 @@ def create_csv(csv_file: str):
     return csv_file
 
 
-def add_class(csv_file: str, class_name: str):
+def add_class(csv_file: str, class_name: str, video_input: str):
     """
     Function to CREATE new class in the csv file or ADD data to existing class
 
+    :param video_input: Choose if phone or webcam
     :param csv_file: Name of the file
     :param class_name: Name of the class that you want to add data to
     :return: None
@@ -44,12 +46,22 @@ def add_class(csv_file: str, class_name: str):
     mp_drawing = mp.solutions.drawing_utils  # Drawing helpers
     mp_holistic = mp.solutions.holistic  # Mediapipe Solutions
 
+    # Replace the below URL with your own. Make sure to add "/shot.jpg" at last.
+    url = "http://192.168.0.22:8080/shot.jpg"
+
     # Takes video camera input to fill file
     cap = cv2.VideoCapture(0)
+
     # Initiate holistic model
     with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
-        while cap.isOpened():
-            ret, frame = cap.read()
+        while True:
+            if video_input == "phone":
+                img_resp = requests.get(url)
+                img_arr = np.array(bytearray(img_resp.content), dtype=np.uint8)
+                frame = cv2.imdecode(img_arr, -1)
+
+            if video_input == "webcam":
+                ret, frame = cap.read()
 
             # Recolor Feed
             image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -122,7 +134,9 @@ def add_class(csv_file: str, class_name: str):
             if cv2.waitKey(10) & 0xFF == ord('q'):
                 break
 
-    cap.release()
+    if video_input == "webcam":
+        cap.release()
+
     cv2.destroyAllWindows()
 
     return None
@@ -196,14 +210,14 @@ def get_only_binary_file(fit_models: dict):
     return binary_file
 
 
-def main(evaluate: bool, existing_csv_file=None, class_name=None, new_csv_file=False, add_class_only=False):
+def main(evaluate: bool, video_input: str, existing_csv_file=None, class_name=None, new_csv_file=False, add_class_only=False):
     if add_class_only:
         """Only adds data to the csv file to make it faster and load many data at once"""
         # Add data to existing file
         csv_file = existing_csv_file
 
         # Add a class
-        add_class(csv_file, class_name)
+        add_class(csv_file, class_name, video_input)
         print(f"Class {class_name} was added to {csv_file} without updating the binary file")
 
     elif not class_name:
@@ -234,7 +248,7 @@ def main(evaluate: bool, existing_csv_file=None, class_name=None, new_csv_file=F
             csv_file = create_csv("../modules/coords.csv")
 
             # Add a class to new csv file
-            add_class(csv_file, class_name)
+            add_class(csv_file, class_name, video_input)
             print(f"Class {class_name} added to {csv_file}")
 
         else:
@@ -242,7 +256,7 @@ def main(evaluate: bool, existing_csv_file=None, class_name=None, new_csv_file=F
             csv_file = existing_csv_file
 
             # Add a class
-            add_class(csv_file, class_name)
+            add_class(csv_file, class_name, video_input)
             print(f"Class {class_name} added to {csv_file} \n")
             print("Training models...")
 
@@ -289,13 +303,15 @@ if __name__ == "__main__":
 
     """Examples of use"""
     # If creating new csv file or delete existing and start over (remember that you need at least 2 classes)
-    # main(new_csv_file=True, evaluate=False, class_name="BigSmile")
+    # main(new_csv_file=True, evaluate=False, class_name="Normal", video_input="phone")
 
     # If csv file already exists
-    main(existing_csv_file="../modules/coords.csv", evaluate=False, class_name="Normal", add_class_only=False)
+    main(existing_csv_file="../modules/coords.csv", evaluate=False, class_name="Sonriendo",
+         add_class_only=False, video_input="phone")
 
     # To just add data to the csv and update binary file later (to make it faster)
-    # main(existing_csv_file="modules/coords.csv", evaluate=False, class_name="BigSmile", add_class_only=True)
+    # main(existing_csv_file="modules/coords.csv", evaluate=False, class_name="Normal",
+    #      add_class_only=True, video_input="phone")
 
     # To just train and test the models and get the binary files (if you have previously added a class only)
-    # main(existing_csv_file="modules/coords.csv", evaluate=False, add_class_only=False)
+    # main(existing_csv_file="../modules/coords.csv", evaluate=False, add_class_only=False, video_input="phone")
