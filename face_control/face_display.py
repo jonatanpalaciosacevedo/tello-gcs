83937a1711779ea37953b4d3f08d7f8f37063c6f
Jon <jonatangpalacios@gmail.com>
2022-07-27 09:04:54 +0200

    Updates

--- face_control/face_display.py
+++ face_control/face_display.py
@@ -3,114 +3,144 @@ import pandas as pd
 import pickle
 import cv2
 import numpy as np
+import requests
 
-mp_drawing = mp.solutions.drawing_utils  # Drawing helpers
-mp_holistic = mp.solutions.holistic  # Mediapipe Solutions
 
-with open('../modules/body_language.pkl', 'rb') as f:
-    model = pickle.load(f)
+def main(video_input: str, ip=None):
 
-# Get webcam feed
-cap = cv2.VideoCapture(0)
+    if video_input != "webcam" or video_input != "phone":
+        return "Error, invalid video_input"
 
-# Initiate holistic model
-with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
-    while cap.isOpened():
-        ret, frame = cap.read()  # PARA WEBCAM
+    if video_input == "phone":
+        if ip is None:
+            return "Error, if video_input is phone, please enter a valid ip address from ip webcam"
 
-        # Recolor Feed
-        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        image.flags.writeable = False
+    mp_drawing = mp.solutions.drawing_utils  # Drawing helpers
+    mp_holistic = mp.solutions.holistic  # Mediapipe Solutions
 
-        # Make Detections
-        results = holistic.process(image)
+    with open('../modules/body_language.pkl', 'rb') as f:
+        model = pickle.load(f)
 
-        # Recolor image back to BGR for rendering
-        image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    # Get webcam feed
+    cap = cv2.VideoCapture(0)
 
-        # 1. Draw face landmarks
-        mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION,
-                                  mp_drawing.DrawingSpec(color=(80, 110, 10), thickness=1, circle_radius=1),
-                                  mp_drawing.DrawingSpec(color=(80, 256, 121), thickness=1, circle_radius=1)
-                                  )
+    # Replace the below URL with your own. Make sure to add "/shot.jpg" at last.
+    url = f"http://{ip}:8080/shot.jpg"
 
-        # 2. Right hand
-        mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
-                                  mp_drawing.DrawingSpec(color=(80, 22, 10), thickness=2, circle_radius=4),
-                                  mp_drawing.DrawingSpec(color=(80, 44, 121), thickness=2, circle_radius=2)
-                                  )
+    if video_input == "webcam":
+        # Takes video camera input to fill file
+        cap = cv2.VideoCapture(0)
 
-        # 3. Left Hand
-        mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
-                                  mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
-                                  mp_drawing.DrawingSpec(color=(121, 44, 250), thickness=2, circle_radius=2)
-                                  )
+    # Initiate holistic model
+    with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+        while True:
+            if video_input == "phone":
+                img_resp = requests.get(url)
+                img_arr = np.array(bytearray(img_resp.content), dtype=np.uint8)
+                frame = cv2.imdecode(img_arr, -1)
 
-        # 4. Pose Detections
-        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
-                                  mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=4),
-                                  mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
-                                  )
-        # Export coordinates
-        try:
-            # Extract Pose landmarks
-            pose = results.pose_landmarks.landmark
-            pose_row = list(
-                np.array([[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in pose]).flatten())
+            if video_input == "webcam":
+                ret, frame = cap.read()
 
-            # Extract Face landmarks
-            face = results.face_landmarks.landmark
-            face_row = list(
-                np.array([[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in face]).flatten())
-
-            # Concatenate rows
-            row = pose_row + face_row
+            # Recolor Feed
+            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            image.flags.writeable = False
 
             # Make Detections
-            X = pd.DataFrame([row])
-            body_language_class = model.predict(X)[0]
-            body_language_prob = model.predict_proba(X)[0]
-            print(body_language_class, body_language_prob)
+            results = holistic.process(image)
 
-            # Grab ear coords
-            coords = tuple(np.multiply(
-                np.array(
-                    (results.pose_landmarks.landmark[mp_holistic.PoseLandmark.LEFT_EAR].x,
-                     results.pose_landmarks.landmark[mp_holistic.PoseLandmark.LEFT_EAR].y))
-                , [640, 480]).astype(int))
+            # Recolor image back to BGR for rendering
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-            cv2.rectangle(image,
-                          (coords[0], coords[1] + 5),
-                          (coords[0] + len(body_language_class) * 20, coords[1] - 30),
-                          (245, 117, 16), -1)
-            cv2.putText(image, body_language_class, coords,
-                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+            # 1. Draw face landmarks
+            mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION,
+                                      mp_drawing.DrawingSpec(color=(80, 110, 10), thickness=1, circle_radius=1),
+                                      mp_drawing.DrawingSpec(color=(80, 256, 121), thickness=1, circle_radius=1)
+                                      )
 
-            # Get status box
-            cv2.rectangle(image, (0, 0), (250, 60), (245, 117, 16), -1)
+            # 2. Right hand
+            mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                                      mp_drawing.DrawingSpec(color=(80, 22, 10), thickness=2, circle_radius=4),
+                                      mp_drawing.DrawingSpec(color=(80, 44, 121), thickness=2, circle_radius=2)
+                                      )
 
-            # Display Class
-            cv2.putText(image, 'CLASS'
-                        , (95, 12), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
-            cv2.putText(image, body_language_class.split(' ')[0]
-                        , (90, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+            # 3. Left Hand
+            mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                                      mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
+                                      mp_drawing.DrawingSpec(color=(121, 44, 250), thickness=2, circle_radius=2)
+                                      )
 
-            # Display Probability
-            cv2.putText(image, 'PROB'
-                        , (15, 12), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
-            cv2.putText(image, str(round(body_language_prob[np.argmax(body_language_prob)], 2))
-                        , (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+            # 4. Pose Detections
+            mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
+                                      mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=4),
+                                      mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
+                                      )
+            # Export coordinates
+            try:
+                # Extract Pose landmarks
+                pose = results.pose_landmarks.landmark
+                pose_row = list(
+                    np.array(
+                        [[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in pose]).flatten())
 
-        except:
-            pass
+                # Extract Face landmarks
+                face = results.face_landmarks.landmark
+                face_row = list(
+                    np.array(
+                        [[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in face]).flatten())
 
-        cv2.imshow('Webcam Feed', image)
+                # Concatenate rows
+                row = pose_row + face_row
 
-        if cv2.waitKey(10) & 0xFF == ord('q'):
-            break
+                # Make Detections
+                X = pd.DataFrame([row])
+                body_language_class = model.predict(X)[0]
+                body_language_prob = model.predict_proba(X)[0]
+                print(body_language_class, body_language_prob)
 
-cap.release()
-cv2.destroyAllWindows()
+                # Grab ear coords
+                coords = tuple(np.multiply(
+                    np.array(
+                        (results.pose_landmarks.landmark[mp_holistic.PoseLandmark.LEFT_EAR].x,
+                         results.pose_landmarks.landmark[mp_holistic.PoseLandmark.LEFT_EAR].y))
+                    , [640, 480]).astype(int))
+
+                cv2.rectangle(image,
+                              (coords[0], coords[1] + 5),
+                              (coords[0] + len(body_language_class) * 20, coords[1] - 30),
+                              (245, 117, 16), -1)
+                cv2.putText(image, body_language_class, coords,
+                            cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
+                # Get status box
+                cv2.rectangle(image, (0, 0), (250, 60), (245, 117, 16), -1)
+
+                # Display Class
+                cv2.putText(image, 'CLASS'
+                            , (95, 12), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+                cv2.putText(image, body_language_class.split(' ')[0]
+                            , (90, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
+                # Display Probability
+                cv2.putText(image, 'PROB'
+                            , (15, 12), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+                cv2.putText(image, str(round(body_language_prob[np.argmax(body_language_prob)], 2))
+                            , (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
+            except:
+                pass
+
+            cv2.imshow('Webcam Feed', image)
+
+            if cv2.waitKey(10) & 0xFF == ord('q'):
+                break
+
+    cap.release()
+    cv2.destroyAllWindows()
 
 
+if __name__ == "__main__":
+    main(video_input="webcam")
+
+    # main(video_input="phone", ip="192.168.0.11")
